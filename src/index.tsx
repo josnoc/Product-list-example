992229820ccb1c8d8f474f0844aeaba1d9f56281
Joseph Enoch <josnocpp@protonmail.com>
2019-10-18 18:37:55 -0500

    Added TODOS for productization.

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,10 @@ import "./index.scss";
 
 import Router from "./router";
 
+// TODO: Add Unit Testing.
+// TODO: Add Application Global State.
+// TODO: If Project will scale, add some CSS methodology like BEM for better style handling.
+// TODO: export to gzip file to reduce loadTimes.
+// TODO: separate chunks for lazy loading.
+
 ReactDOM.render(<Router />, document.getElementById("app"));
