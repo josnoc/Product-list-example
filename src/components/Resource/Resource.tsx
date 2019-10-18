3c97a45613b41254f58f8bf6623ee523223c918d
Joseph Enoch <josnocpp@protonmail.com>
2019-10-18 18:08:39 -0500

    Added Prop passing functionality and improved readability.

--- src/components/Resource/Resource.tsx
+++ src/components/Resource/Resource.tsx
@@ -1,34 +1,35 @@
 import axios from "axios";
 import * as React from "react";
 
-interface IResourceProps<R, E> {
+interface IResourceProps<R, E, P = {}> {
   path: string;
-  render: (state: IResourceState<R, E>) => React.ReactNode;
+  render: (state: IResourceState<R, E>, props?: P) => React.ReactNode;
+  props?: P;
 }
 
 export interface IResourceState<R, E> {
-  loading: boolean;
+  isLoading: boolean;
   error: E;
   response: R;
 }
 
-export default class Resource<R, E> extends React.Component<
-  IResourceProps<R, E>,
+export default class Resource<R, E, P = {}> extends React.Component<
+  IResourceProps<R, E, P>,
   IResourceState<R, E>
 > {
   async componentDidMount() {
     try {
-      this.setState({ loading: true });
+      this.setState({ isLoading: true });
       const response = (await axios.get(this.props.path)).data;
-      this.setState({ loading: false, response });
+      this.setState({ isLoading: false, response });
     } catch (e) {
-      this.setState({ loading: false, error: e });
+      this.setState({ isLoading: false, error: e });
     }
   }
 
   render() {
     if (!this.state) return null;
 
-    return this.props.render(this.state);
+    return this.props.render(this.state, this.props.props);
   }
 }
